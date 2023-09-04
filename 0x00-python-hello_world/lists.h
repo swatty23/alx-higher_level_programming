3e539f92a7415719c5a4bf9e17102a6fe47bf401
swatty23 <swattymovine@gmail.com>
2023-09-04 03:00:12 -0700

    0x00. Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list node
- * @n: integer value stored in the node
- * @next: points to the next node in the list
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
